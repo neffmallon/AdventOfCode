0b203e0c8b6608550f0a50fc572f8299f77c767c
Nathan Neff-Mallon <mallonna@gmail.com>
2021-11-28 09:38:47 -0600

    Completed day 3

--- day_03.py
+++ day_03.py
@@ -1,32 +1,97 @@
-import pytest
+def wire_cross_finder(wire_list: list):
+    wire_points = dict()
+    for wire_idx, wire in enumerate(wire_list):
+        occupied_points = set()
+        current_location = [0, 0]
+        for segment in wire.strip().split(","):
+            direction = segment[0]
+            distance = int(segment[1:])
+            if direction in ("R", "L"):
+                axis = 0
+                if direction == "L":
+                    distance = 0 - distance
+            elif direction in ("U", "D"):
+                axis = 1
+                if direction == "D":
+                    distance = 0 - distance
+            else:
+                raise ValueError(f"{segment} is not a valid wire")
+
+            if distance < 0:
+                direction = -1
+            else:
+                direction = 1
+
+            for step in range(0, distance, direction):
+                current_location[axis] += direction
+                occupied_points.add(tuple(current_location))
+        wire_points[wire_idx] = occupied_points
+
+    crossed_points = wire_points[0].intersection(wire_points[1])
+
+    min_dist = None
+    for point in crossed_points:
+        if min_dist is None:
+            min_dist = abs(point[0]) + abs(point[1])
+        elif point == (0, 0):
+            continue
+        else:
+            min_dist = min(abs(point[0]) + abs(point[1]), min_dist)
+
+    return min_dist
 
 
-def wire_cross_finder(input: str):
-    raise NotImplemented()
+def wire_cross_finder_wire_distance(wire_list: list):
 
+    wire_points = dict()
+    wire_dists = dict()
+    for wire_idx, wire in enumerate(wire_list):
+        occupied_points = set()
+        occupied_distances = dict()
+        current_location = [0, 0]
+        current_wire_length = 0
+        for segment in wire.strip().split(","):
+            direction = segment[0]
+            distance = int(segment[1:])
+            if direction in ("R", "L"):
+                axis = 0
+                if direction == "L":
+                    distance = 0 - distance
+            elif direction in ("U", "D"):
+                axis = 1
+                if direction == "D":
+                    distance = 0 - distance
+            else:
+                raise ValueError(f"{segment} is not a valid wire")
 
-@pytest.fixture()
-def easy_test():
-    """answer is 6"""
-    return "R8,U5,L5,D3\nU7,R6,D4,L4", 6
+            direction = distance // abs(distance)
+            assert direction in (1, -1)
 
+            for step in range(0, distance, direction):
+                current_location[axis] += direction
+                current_wire_length += 1
+                if not tuple(current_location) in occupied_points:
+                    occupied_points.add(tuple(current_location))
+                    occupied_distances[tuple(current_location)] = current_wire_length
 
-@pytest.fixture()
-def med_test():
-    """answer is 159"""
-    return "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83", 159
+        wire_points[wire_idx] = occupied_points
+        wire_dists[wire_idx] = occupied_distances
 
+    crossed_points = wire_points[0].intersection(wire_points[1])
 
-@pytest.fixture()
-def hard_test():
-    """answer is 135"""
-    return (
-        "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
-        135,
-    )
+    min_dist = None
+    for point in crossed_points:
+        if min_dist is None:
+            min_dist = wire_dists[0][point] + wire_dists[1][point]
+        elif point == (0, 0):
+            continue
+        else:
+            min_dist = min(wire_dists[0][point] + wire_dists[1][point], min_dist)
 
+    return min_dist
 
-def test_wire_cross_finder(easy_test, med_test, hard_test):
-    assert wire_cross_finder(easy_test[0]) == easy_test[1]
-    assert wire_cross_finder(med_test[0]) == med_test[1]
-    assert wire_cross_finder(hard_test[0]) == hard_test[1]
+if __name__ == "__main__":
+    with open("day_03_input.txt", 'r') as f:
+        wire_map = f.readlines()
+    print(f"Part 1: {wire_cross_finder(wire_map)}")
+    print(f"Part 2: {wire_cross_finder_wire_distance(wire_map)}")
---
+++ test_day_03.py
@@ -0,0 +1,47 @@
+import pytest
+from day_03 import wire_cross_finder, wire_cross_finder_wire_distance
+
+
+@pytest.fixture()
+def easy_test():
+    """answer is 6"""
+    return "R8,U5,L5,D3\nU7,R6,D4,L4", 6
+
+
+@pytest.fixture()
+def med_test():
+    """answer is 159"""
+    return "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83", 159
+
+
+@pytest.fixture()
+def hard_test():
+    """answer is 135"""
+    return (
+        "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+        135,
+    )
+
+
+def test_easy_wire_cross_finder(easy_test):
+    assert wire_cross_finder(easy_test[0].split("\n")) == easy_test[1]
+
+
+def test_med_wire_cross_finder(med_test):
+    assert wire_cross_finder(med_test[0].split("\n")) == med_test[1]
+
+
+def test_hard_wire_cross_finder(hard_test):
+    assert wire_cross_finder(hard_test[0].split("\n")) == hard_test[1]
+
+
+def test_easy_wire_cross_finder_wire_distance(easy_test):
+    assert wire_cross_finder_wire_distance(easy_test[0].split("\n")) == 30
+
+
+def test_med_wire_cross_finder_wire_distance(med_test):
+    assert wire_cross_finder_wire_distance(med_test[0].split("\n")) == 610
+
+
+def test_hard_wire_cross_finder_wire_distance(hard_test):
+    assert wire_cross_finder_wire_distance(hard_test[0].split("\n")) == 410
