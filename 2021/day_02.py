54c2c37aabf600178b192f4c6701d0640d4d45e1
Nathan Neff-Mallon <mallonna@gmail.com>
2021-12-01 23:11:24 -0600

    Solution to 2021 day 2

---
+++ 2021/day_02.py
@@ -0,0 +1,39 @@
+with open("2021/day_02_in.txt", "r") as f:
+    puzzle_input = [s.strip() for s in f]
+
+depth = 0
+x_pos = 0
+
+instructions = [s.split(" ") for s in puzzle_input]
+
+for i in instructions:
+    if i[0] == "forward":
+        x_pos += int(i[1])
+    elif i[0] == "down":
+        depth += int(i[1])
+    elif i[0] == "up":
+        depth -= int(i[1])
+print(f"Part 1: {depth*x_pos}")
+
+
+with open("2021/day_02_in.txt", "r") as f:
+    puzzle_input = [s.strip() for s in f]
+
+depth = 0
+x_pos = 0
+aim = 0
+
+instructions = [s.split(" ") for s in puzzle_input]
+
+for i in instructions:
+    if i[0] == "forward":
+        x_pos += int(i[1])
+        depth += aim * int(i[1])
+    elif i[0] == "down":
+        aim += int(i[1])
+    elif i[0] == "up":
+        aim -= int(i[1])
+print(f"Part 2: {depth*x_pos}")
+
+if __name__ == "__main__":
+    pass
