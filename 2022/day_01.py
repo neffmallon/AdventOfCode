7e423ee73825cd1ecc9f2b573efca39f00d8853e
Nathan NEff-Mallon <mallon.na@gmail.com>
2022-12-08 23:31:06 -0600

    Day 1 of 2022

---
+++ 2022/day_01.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+project_dir = Path(__file__).resolve().parents[1]
+file = os.path.join(project_dir, "2022", "day_01_in.txt")
+with open(file, "r") as f:
+    puzzle_input = [s.strip() for s in f]
+
+elves = []
+elf = 0
+for snack in puzzle_input:
+    if snack == '':
+        elves.append(elf)
+        elf = 0
+    else:
+        elf += int(snack)
+elves.sort()
+print(f"Part 1: {elves[-1]}")
+print(f"Part 2: {sum(elves[-3:])}")
